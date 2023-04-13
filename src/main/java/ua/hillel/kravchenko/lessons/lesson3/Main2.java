270c4e015af39fa1b5b692fae601ca92ae299e5b
Dmytro Kravchenko <melpwnz@gmail.com>
2023-04-13 18:48:49 +0300

    main lesson3

--- src/main/java/ua/hillel/kravchenko/lessons/lesson3/Main2.java
+++ src/main/java/ua/hillel/kravchenko/lessons/lesson3/Main2.java
@@ -3,5 +3,9 @@ package ua.hillel.kravchenko.lessons.lesson3;
 public class Main2 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
-}git
+}
